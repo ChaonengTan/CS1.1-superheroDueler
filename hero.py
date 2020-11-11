b830d01ef7f4f6b1dc29d79841e03ab3f80a70fc
Chaoneng Tan <69783753+ChaonengTan@users.noreply.github.com>
2020-11-10 19:04:17 -0800

    +defs: attack, add_armor, defend, take_damage, is_alive

--- hero.py
+++ hero.py
@@ -13,13 +13,29 @@ class Hero:
         print(random.choice(heros),"wins!")
     def add_ability(self, ability):
         self.abilities.append(ability)
+    def attack(self):
+        total_damage=0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+    def add_armor(self, armor):
+        self.armors.append(armor)
+    def defend(self):
+        totalBlock=0
+        for armor in self.armors:
+            totalBlock+=armor.block()
+        return totalBlock
+    def take_damage(self, damage):
+        self.current_health-=(damage-self.defend())
+    def is_alive(self):  
+        if self.current_health>0:
+            return True
+        else:
+            return False
+
 # Testing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-hero1=Hero("Grace Hopper", 200)
-hero2=Hero("bob", 100)
-hero1.fight(hero2)
-ability = Ability("Debugging Ability", 20)
-print(ability.name)
-print(ability.attack())
-armor = Armor("Debugging Shield", 10)
-print(armor.name)
-print(armor.block())
+hero = Hero("Grace Hopper", 200)
+hero.take_damage(150)
+print(hero.is_alive())
+hero.take_damage(15000)
+print(hero.is_alive())
